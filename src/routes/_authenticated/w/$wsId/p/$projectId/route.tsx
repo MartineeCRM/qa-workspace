15e773ef00083b004326c5a416753de1f3d56318
aimed <aimed@Yoomin-Jeong-MacBook-Air.local>
2026-09-15 13:36:34 +0900

    ui: reorder QA tabs and rename issue management

--- src/routes/_authenticated/w/$wsId/p/$projectId/route.tsx
+++ src/routes/_authenticated/w/$wsId/p/$projectId/route.tsx
@@ -102,9 +102,6 @@ function ProjectShell() {
           <TabLink to="/w/$wsId/p/$projectId" params={{ wsId, projectId }} exact>
             개요
           </TabLink>
-          <TabLink to="/w/$wsId/p/$projectId/issues" params={{ wsId, projectId }}>
-            이슈 모아보기
-          </TabLink>
           {(stages ?? []).map((stage: QaEnvironment) => (
             <TabLink
               key={stage.id}
@@ -114,6 +111,9 @@ function ProjectShell() {
               {stage.name}
             </TabLink>
           ))}
+          <TabLink to="/w/$wsId/p/$projectId/issues" params={{ wsId, projectId }}>
+            이슈 관리
+          </TabLink>
           <TabLink to="/w/$wsId/p/$projectId/taxonomy" params={{ wsId, projectId }}>
             택소노미
           </TabLink>
